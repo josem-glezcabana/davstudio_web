d7e572ccaa2060a3e3782ab15903d83a82d5207d
José Manuel González Cabana <josemcabana9@gmail.com>
2026-01-06 21:40:12 +0100

    WIP: añadir páxinas losta de productos e contacto

---
+++ src/components/contact/contact.js
@@ -0,0 +1,18 @@
+export default function contact() {
+    return (
+        <div className='flex flex-column justify-content-center text-center'>
+            <p className="m-4 line-height-3 text-justify text-xl">
+                ¡Estamos aquí para ayudarte! En DAVSTUDIO, valoramos la comunicación con nuestros clientes y estamos comprometidos en brindarte el mejor servicio posible. Si tienes alguna pregunta, inquietud o simplemente deseas obtener más información sobre nuestros productos y servicios, no dudes en ponerte en contacto con nosotros.
+            </p>
+            <p className="m-4 line-height-3 text-justify text-xl">
+                Puedes enviarnos un correo electrónico a contact@davstudio.com o llamarnos al +34 123 456 789. Nuestro equipo de atención al cliente estará encantado de asistirte y proporcionarte la información que necesitas.
+            </p>
+            <p className="m-4 line-height-3 text-justify text-xl">
+                También puedes visitarnos en nuestra tienda física ubicada en Calle Ejemplo 123, Ciudad, País. Estamos abiertos de lunes a viernes de 9:00 a 18:00 horas.
+            </p>
+            <p className="m-4 line-height-3 text-justify text-xl">
+                En DAVSTUDIO, tu satisfacción es nuestra prioridad. Esperamos tener noticias tuyas pronto y ayudarte a hacer realidad tus ideas de diseño de interiores.
+            </p>
+        </div>
+    );
+}
---
+++ src/components/products/productList.js
@@ -0,0 +1,15 @@
+export default function ProductList() {
+    return (
+        <div className='flex flex-column justify-content-center text-center'>
+            <p className="m-4 line-height-3 text-justify text-xl">
+                Aquí encontrarás una selección exclusiva de productos de interiorismo diseñados para transformar tus espacios. Desde muebles elegantes hasta accesorios decorativos, cada artículo ha sido cuidadosamente elegido para ofrecer calidad, estilo y funcionalidad.
+            </p>
+            <p className="m-4 line-height-3 text-justify text-xl">
+                Navega por nuestras categorías y descubre las últimas tendencias en diseño de interiores. Ya sea que busques inspiración para tu hogar o soluciones prácticas para tu oficina, en nuestra tienda online encontrarás todo lo que necesitas para crear ambientes únicos y acogedores.
+            </p>
+            <p className="m-4 line-height-3 text-justify text-xl">
+                No dudes en contactarnos si tienes alguna pregunta o necesitas asesoramiento personalizado. En DAVSTUDIO, estamos aquí para ayudarte a hacer realidad tus ideas de diseño.
+            </p>
+        </div>
+    );
+}
