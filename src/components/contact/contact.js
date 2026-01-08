d4fe2a7a89c98d9b2092e8628627f7afc90d02c2
José Manuel González Cabana <josemcabana9@gmail.com>
2026-01-08 20:35:08 +0100

    Cambio de horario no texto de contacto

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -8,7 +8,7 @@ export default function contact() {
                 Puedes enviarnos un correo electrónico a contact@davstudio.com o llamarnos al +34 123 456 789. Nuestro equipo de atención al cliente estará encantado de asistirte y proporcionarte la información que necesitas.
             </p>
             <p className="m-4 line-height-3 text-justify text-xl">
-                También puedes visitarnos en nuestra tienda física ubicada en Calle Ejemplo 123, Ciudad, País. Estamos abiertos de lunes a viernes de 9:00 a 18:00 horas.
+                También puedes visitarnos en nuestra tienda física ubicada en Calle Ejemplo 123, Ciudad, País. Estamos abiertos de lunes a viernes de 13:00 a 14:00 horas.
             </p>
             <p className="m-4 line-height-3 text-justify text-xl">
                 En DAVSTUDIO, tu satisfacción es nuestra prioridad. Esperamos tener noticias tuyas pronto y ayudarte a hacer realidad tus ideas de diseño de interiores.
